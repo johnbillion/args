9cee4b2e61a42336bf7f1126f820c70a26445c7d
John Blackbourn <johnbillion@gmail.com>
2021-06-20 16:55:24 +0100

    Add definitions for `wp_insert_post()` and `wp_update_post()`.

---
+++ src/Args/wp_update_post.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Args;
+
+/**
+ * Arguments for the `wp_update_post()` function in WordPress.
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_update_post/
+ * @link https://developer.wordpress.org/reference/functions/wp_insert_post/
+ */
+class wp_update_post extends wp_insert_post {
+}
