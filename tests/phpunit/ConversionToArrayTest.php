eaacd84ea21a5132172a315719a5cfdfb1e1dd31
John Blackbourn <john@humanmade.com>
2021-12-17 15:52:46 +0100

    Ensure unknown parameters are included in the exported array.

--- tests/phpunit/ConversionToArrayTest.php
+++ tests/phpunit/ConversionToArrayTest.php
@@ -13,12 +13,33 @@ final class ConversionToArrayTest extends TestCase {
 	public function testBasicParamsAreCorrectlyConvertedToArray(): void {
 		$args = new \Args\WP_Query;
 
+		self::assertObjectHasAttribute( 'attachment_id', $args );
+		self::assertObjectHasAttribute( 'author', $args );
+
 		$args->attachment_id = 123;
 		$args->author = '1,2,3';
 
 		$expected = [
 			'attachment_id' => 123,
 			'author' => '1,2,3',
+		];
+		$actual = $args->toArray();
+
+		self::assertSame( $expected, $actual );
+	}
+
+	public function testUnknownParamsAreIncludedInArray(): void {
+		$args = new \Args\WP_Query;
+
+		self::assertObjectHasAttribute( 'attachment_id', $args );
+		self::assertObjectNotHasAttribute( 'hello', $args );
+
+		$args->attachment_id = 123;
+		$args->hello = 'world';
+
+		$expected = [
+			'attachment_id' => 123,
+			'hello' => 'world',
 		];
 		$actual = $args->toArray();
 
