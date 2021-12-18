3d95dd4d845e48e9fac43210363d65c956f4d90d
John Blackbourn <john@humanmade.com>
2021-12-18 12:43:19 +0100

    Clean up `generate.php`. Fixes #15.

--- generate.php
+++ generate.php
@@ -1,12 +1,20 @@
 #!/usr/bin/env php
 <?php
 
-use phpDocumentor\Reflection\DocBlock\Tags\BaseTag;
+use phpDocumentor\Reflection\DocBlock\Tag;
 use phpDocumentor\Reflection\DocBlock\Tags\Param;
 use phpDocumentor\Reflection\Php\Project;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+/**
+ * @var array{
+ *   file?: string,
+ *   method?: string,
+ *   function?: string,
+ *   param?: string,
+ * } $options
+ */
 $options = getopt( '', [
 	'file:',
 	'method::',
@@ -78,7 +86,7 @@ if ( ! empty( $options['method'] ) ) {
 
 	$symbol = $methods[ $options['method'] ];
 	$name = $oc;
-} else {
+} elseif ( ! empty( $options['function'] ) ) {
 	$functions = $file->getFunctions();
 
 	if ( ! isset( $functions[ $options['function'] ] ) ) {
@@ -91,16 +99,25 @@ if ( ! empty( $options['method'] ) ) {
 
 	$symbol = $functions[ $options['function'] ];
 	$name = trim( $options['function'], '()' );
+} else {
+	echo 'No function or method provided';
+	exit( 1 );
 }
 
-$tags = $symbol->getDocBlock()->getTags();
+$docBlock = $symbol->getDocBlock();
 
-/** @var BaseTag[] $tags */
-$tags = array_values( array_filter( $tags, function( BaseTag $tag ) : bool {
+if ( $docBlock === null ) {
+	echo 'Function docblock not found';
+	exit( 1 );
+}
+
+$tags = $docBlock->getTags();
+
+/** @var Param[] $tags */
+$tags = array_values( array_filter( $tags, function( Tag $tag ) : bool {
 	return ( $tag instanceof Param );
 } ) );
 
-/** @var Param[] $params */
 $params = array_values( array_filter( $tags, function( Param $tag ) use ( $options ) : bool {
 	return (string) $tag->getVariableName() === $options['param'];
 } ) );
@@ -114,19 +131,18 @@ if ( empty( $params ) ) {
 }
 
 $desc = (string) $params[0]->getDescription();
-$desc = trim( $desc, '{' );
-$desc = trim( $desc, '}' );
+$desc = trim( $desc, '{}' );
 $desc = explode( '@type', $desc );
 $desc = array_map( 'trim', $desc );
 
 unset( $desc[0] );
 
 $desc = array_map( function( string $string ) : array {
-	return preg_split( '#\s+#', $string, 3 );
+	return (array) preg_split( '#\s+#', $string, 3 );
 }, $desc );
 
 $desc = array_map( function( array $item ) : string {
-	$item[2] = preg_replace( '#\n\s+#', ' ', $item[2] );
+	$item[2] = preg_replace( '#\n\s+#', ' ', (string) $item[2] );
 
 	return sprintf(
 		<<<'BLOCK'
